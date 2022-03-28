68dd356be29aaa95928c723ce3d4cea965b42265
ApexioDaCoder <apexio@hey.com>
2022-03-28 02:12:11 +0000

    faster with cache

--- pages/api/search/[pid].js
+++ pages/api/search/[pid].js
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import ytsr from "ytsr"
-const limit = process.env.LIMIT || 50;
+import Cache from "node-cache"
+
+const limit = process.env.LIMIT || 25;
+const cacheTime = process.env.CACHE_TIME || 3;
+const cache = new Cache({ stdTTL: cacheTime * 1000 * 60 * 60 })
 
 export default async function handler(req, res) {
   const { pid: query } = req.query
   if (!query) return res.status(418).text("A search query is required.")
 
   try {
+    if (cache.has(query)) return res.status(200).json(cache.get(query))
     const results = await ytsr(query, { limit })
+    cache.set(query, results)
     res.status(200).json(results)
   } catch (e) {
     console.log(e)
--- pages/search/[pid].js
+++ pages/search/[pid].js
@@ -3,48 +3,60 @@ import useSWR from "swr";
 import axios from "axios";
 import VideoPreview from "@components/preview/video";
 import { Grid, Loading, Text, Spacer } from "@nextui-org/react";
-import Head from 'next/head'
-import { useState, useEffect } from "react"
+import Head from "next/head";
+import { useState, useEffect } from "react";
 
-const SearchPage = () => {
+import ytsr from "ytsr";
+import Cache from "node-cache";
+
+const limit = process.env.LIMIT || 25;
+const cacheTime = process.env.CACHE_TIME || 3;
+const cache = new Cache({ stdTTL: cacheTime * 1000 * 60 * 60 });
+
+export async function getServerSideProps({ params }) {
+  const query = params.pid;
+
+    if (cache.has(query))
+      return { props: { results: JSON.stringify(cache.get(query)) } };
+    const results = await ytsr(query, { limit });
+    cache.set(query, results);
+    return { props: { results: JSON.stringify(results)}};
+}
+
+const SearchPage = ({ results }) => {
   const router = useRouter();
   const { pid: search } = router.query;
+  const data = JSON.parse(results);
 
-  const fetcher = (url) => axios.get(url).then((res) => res.data);
-  /*const { data, error } = useSWR("/api/search/" + search, fetcher, {
-    revalidateOnFocus: false,
-    revalidateOnMount: false,
-    revalidateOnReconnect: false,
-    refreshWhenOffline: false,
-    refreshWhenHidden: false,
-    refreshInterval: 0
-  });*/
-
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(false)
-
-  useEffect(async ()=>{
-    setData(null)
-    try {
-      setData(await fetcher('/api/search/'+search))
-      console.log(data)
-    } catch(e) {
-      setError(e)
-    }
-  }, [search])
-
-  if (error) return <div>Failed to load</div>;
-  if (!data) return <><Spacer y={1} /><Loading css={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }} /></>;
+  if (!data)
+    return (
+      <>
+        <Spacer y={1} />
+        <Loading
+          css={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        />
+      </>
+    );
   if (data)
     return (
       <>
         <Head>
           <title>Search results for &quot;{search}&quot;</title>
         </Head>
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: "center" }}>
           <Spacer y={0.3} />
-          <Text size={25} weight="bold">Search results for &quot;{search}&quot;</Text>
-          <Grid.Container gap={2} justify="center" style={{ textAlign: 'left' }}>
+          <Text size={25} weight="bold">
+            Search results for &quot;{search}&quot;
+          </Text>
+          <Grid.Container
+            gap={2}
+            justify="center"
+            style={{ textAlign: "left" }}
+          >
             {data.items.map((v, i) => {
               if (v.type == "video") {
                 return (
